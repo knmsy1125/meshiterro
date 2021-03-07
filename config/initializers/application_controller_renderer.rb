a6875c101d9ee5b922535e0e2ba9618e14b2a6e0
knmsy1125 <knmsy1125@gmail.com>
2021-03-07 04:25:42 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '19d101f30b19421bae437e77e43c557423f53c7f03eb97c5582a98e9f371a1636df00b3b673d65bdcf82283d60af40881e29554cb591be4d3b147bf89ccee2d7'
